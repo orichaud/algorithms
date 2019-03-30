8165a8dc12e50a66319cc070877ac07dc56a9a46
Olivier Richaud <orichaud@gmail.com>
2019-03-30 19:48:46 +0100

    simplified example

--- coroutines/cooperative_boost.cpp
+++ coroutines/cooperative_boost.cpp
@@ -1,52 +1,27 @@
 #include <boost/coroutine2/all.hpp>
 #include <iostream>
-#include <map>
 
 // This requires the installation of boost
-// for exampel: brew install boost will solve this
+// for example: brew install boost will solve this
 
 using namespace boost::coroutines2;
 
-typedef int value_t;
-typedef std::string _key_t;
-typedef std::map<_key_t, value_t> map_t;
-
-const map_t&  get_map() noexcept
+void cooperative(coroutine<std::string>::push_type &sink, const std::string &value)
 {
-	static const map_t map = {
-		{ "hello", 1 },
-		{ "world", 2 },
-		{ "it's",  3 },
-		{ "me",    4 },
-	};
-	return map;
-}
+	std::cout << "This is ";
 
-void cooperative(coroutine<value_t>::push_type &sink, const std::string &init)
-{
-	std::cout << "1 ";
-	const auto &map = get_map();
-	auto it = map.find(init);
-	if(it != map.end()) {
-		const auto &value = (*it).second;
-		sink(value);
-	}
-	std::cout << "3 ";
+	sink(value);
+	
+	std::cout << " world!";
 }
 
 int main()
 {
-	for (const auto& [ k, v ] : get_map()) {
-		std::cout << "k=" << k << " v=" << v << '\n';
-	}
-	
-	coroutine<value_t>::pull_type source{ std::bind(cooperative, std::placeholders::_1, "hello") };
-	
+	auto fn = std::bind(cooperative, std::placeholders::_1, "hello");
+	coroutine<std::string>::pull_type source{ fn };
 	auto v = source.get();
-	std::cout << "2(" << v << ") ";
-	
+	std::cout << v;
 	source();
-	
-	std::cout << "!\n";
+	std::cout << std::endl;
 }
 
