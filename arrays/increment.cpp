7009938fc2a4de10a2d53e12130b520e4ca63819
Olivier Richaud <orichaud@gmail.com>
2019-05-18 23:42:33 +0200

    cut all extra steps

--- arrays/increment.cpp
+++ arrays/increment.cpp
@@ -16,30 +16,28 @@ ostream& operator << (ostream& os, const vect_t& vect) {
     return os;
 }
 
-// imcrement an array representing a number by one: {1, 3, 9} becomes {1, 4, 0}
-
+// increment an array representing a number by one: {1, 3, 9} becomes {1, 4, 0}
 vect_t increment(const vect_t& vect) {
     if(vect.empty())
         return {};
 
-    vect_t result(vect.size(), 0); // vector filled with 0
-    result.back()++; // perform increment of 1
-    value_t carry_out = 0; // carry_out to be propagated
-    auto r = result.end() - 1;
-    auto end = vect.rend(); 
-    for(auto it = vect.rbegin(); it != end; it++, r--) {
-        auto v = *r + *it + carry_out;
-        if(v >= 10) { // if carry_out kicks in
-            carry_out = 1;
-            v -= 10;
-        } else {
-            carry_out = 0; // otherwise clear
+    vect_t result(vect.begin(), vect.end()); // vector filled with input
+    // perform increment of 1 of lowest digit and test if propagation is needed
+    if(++result.back() >= 10) { 
+        value_t carry_out = 0; // carry_out to be propagated
+        auto end = result.rend(); 
+        for(auto it = result.rbegin(); it != end; ++it) { // need to go in reverse order
+            *it += carry_out;
+            carry_out = *it >= 10 ? 1 : 0;
+            if(!carry_out)
+                break; // no need to continue and propagate
+            // carry_out kicks in
+            *it -= 10;
         }
-        *r = v;
-    }
-    // add extra element if 
-    if(carry_out > 0) { 
-        result.insert(result.begin(), carry_out);
+        // add extra element if we reach the end and need extra digit
+        if(carry_out > 0) { 
+            result.insert(result.begin(), carry_out);
+        }
     }
     return result;
 }
