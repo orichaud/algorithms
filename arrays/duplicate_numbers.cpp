6d2af6c6b37888b0c2cfe5498755a0e201b7e605
Olivier Richaud <orichaud@gmail.com>
2019-05-19 18:20:50 +0200

    updated bug in duplicates

--- arrays/duplicate_numbers.cpp
+++ arrays/duplicate_numbers.cpp
@@ -4,23 +4,25 @@
 
 using namespace std;
 
-typedef std::vector<int> vect_int;
+typedef int value_t;
+typedef std::vector<value_t> vect_t;
 
 // remove duplicated numbers from array
-
-vect_int remove_duplicates(const vect_int& vect) {
-    unordered_set<int> counters;
-    vect_int result;
-    for(auto v: vect) {
+// no sorting so space is O(number of unique values)
+vect_t remove_duplicates(const vect_t& vect) {
+    unordered_set<value_t> counters;
+    vect_t result;
+    for(const auto& v: vect) {
         auto found = counters.find(v);
         if(found == counters.end()) {
             result.push_back(v);
+            counters.emplace(v);
         }
     }
     return result;
 }
 
-ostream& operator << (ostream& os, const vect_int& vect) {
+ostream& operator << (ostream& os, const vect_t& vect) {
     os << "[ ";
     for(auto v: vect) {
         os << v << " ";
@@ -30,7 +32,7 @@ ostream& operator << (ostream& os, const vect_int& vect) {
 }
 
 int main() {
-    vector<vect_int> vects = {
+    vector<vect_t> vects = {
        {1, 3, 3, 14, 1, 17 },
        {1, 3, 3, 14, 1, 17, 100, 45, 3, 3, 3, 17 }
     };
